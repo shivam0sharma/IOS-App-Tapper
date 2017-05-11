2c267f623d9ad0e141ee8b947e0c6883c6e67b34
Shaisan2 <shivam0sharma@icloud.com>
2017-05-11 16:25:11 -0400

    Made the coin tapping work. Finished the app.

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -16,12 +16,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapButton: UIButton!
     @IBOutlet weak var tapsLabel: UILabel!
     
-    @IBAction func onPlayButtonPressed(_ sender: UIButton) {
+    var numberOfTaps = 0
+    
+    @IBAction func onPlayButtonPressed(_ sender: UIButton!) {
+        if (tapsTextField.hasText) {
+            logoImage.isHidden = true
+            tapsTextField.isHidden = true
+            playButton.isHidden = true
+            
+            tapButton.isHidden = false
+            tapsLabel.isHidden = false
+            
+            numberOfTaps = Int(tapsTextField.text!)!
+            tapsLabel.text = String(numberOfTaps) + " Taps"
+            tapsTextField.resignFirstResponder()
+        }
         
     }
     
-    @IBAction func onTapButtonPressed(_ sender: UIButton) {
+    @IBAction func onTapButtonPressed(_ sender: UIButton!) {
+        if !isGameOver() {
+            numberOfTaps -= 1
+            tapsLabel.text = String(numberOfTaps) + " Taps"
+        } else {
+            restartGame()
+        }
+    }
+    
+    func restartGame() {
+        logoImage.isHidden = false
+        tapsTextField.isHidden = false
+        playButton.isHidden = false
         
+        tapButton.isHidden = true
+        tapsLabel.isHidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if numberOfTaps <= 0 {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
